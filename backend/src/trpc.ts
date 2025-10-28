148598c2dc07996a5739fbb3acc00e59479f5809
Prokopenko Andrey <prokopenkoa@terralink.ru>
2025-10-28 23:16:16 +0300

    add trpc and types

---
+++ backend/src/trpc.ts
@@ -0,0 +1,20 @@
+import { initTRPC } from '@trpc/server'
+
+const persons = [
+  { nick: 'cool-person-nick-1', name: 'Person 1', description: 'Description of person 1...' },
+  { nick: 'cool-person-nick-2', name: 'Person 2', description: 'Description of person 2...' },
+  { nick: 'cool-person-nick-3', name: 'Person 3', description: 'Description of person 3...' },
+  { nick: 'cool-person-nick-4', name: 'Person 4', description: 'Description of person 4...' },
+  { nick: 'cool-person-nick-5', name: 'Person 5', description: 'Description of person 5...' },
+]
+
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getPersons: trpc.procedure.query(() => {
+    throw new Error('Not implemented')
+    return { persons }
+  }),
+})
+
+export type TrpcRouter = typeof trpcRouter
---
+++ webapp/src/pages/AllPersonsPage/index.tsx
@@ -0,0 +1,26 @@
+import { trpc } from '../../lib/trpc.tsx'
+
+export const AllPersonsPage = () => {
+  const { data, error, isLoading, isFetching, isError } = trpc.getPersons.useQuery()
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
+
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+
+  return (
+    <div>
+      <h1>All Persons</h1>
+      {data?.persons.map((person) => {
+        return (
+          <div key={person.nick}>
+            <h2>{person.name}</h2>
+            <p>{person.description}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
